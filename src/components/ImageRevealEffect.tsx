2c6f8949ef1ef1b36f90af3a2351290724a3cb5d
Semih Irmak <smhirmak@hotmail.com>
2023-07-02 22:57:29 +0300

    learned image reveal effect

--- src/components/ImageRevealEffect.tsx
+++ src/components/ImageRevealEffect.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { gsap, Power2 } from 'gsap';
+import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
 
 const ImageRevealEffect = () => {
-  return <div>ImageRevealEffect</div>;
+  let container: any = useRef(null);
+  let image: any = useRef(null);
+  // var imageReveal = CSSRulePlugin.getRule('.img-container:after');
+
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    const GSAP = require('gsap/CSSRulePlugin');
+    const { CSSRulePlugin } = GSAP;
+    gsap.registerPlugin(CSSRulePlugin); //Bu kısım nextjs ile kullanırken cssruleplugin ögesi window a erişmek istediğinden dolayı hata veriyor bu dahil 3 satır ÇÖZÜM
+    // do whatever you want to do with the plugin, its Working now...
+    // for example
+    let imageReveal = CSSRulePlugin.getRule('.img-container:after');
+    tl.to(container, 0, { css: { visibility: 'visible' } })
+      .to(imageReveal, 1.4, {
+        width: '0%',
+        ease: Power2.easeInOut
+      })
+      .from(image, 1.4, { scale: 1.7, ease: Power2.easeInOut, delay: -1.5 });
+  }, []);
+  return (
+    <section className="main">
+      <div className="container" ref={(el) => (container = el)}>
+        <>
+          <div className="img-container">
+            <img ref={(el) => (image = el)} src="macu.webp" alt="" />
+          </div>
+        </>
+      </div>
+    </section>
+  );
 };
 
 export default ImageRevealEffect;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ThreeCicrcle from '@/components/ThreeCircle';
+import ImageRevealEffect from '@/components/ImageRevealEffect';
 
 export default function Home() {
   return (
     <div>
-      <ThreeCicrcle />
+      {/* <ThreeCicrcle /> */}
+      <ImageRevealEffect />
     </div>
   );
 }
